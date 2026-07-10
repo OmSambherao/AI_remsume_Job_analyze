a78d42134cd6026f44e93f9233fb4570395be44f
Om Sambherao <omsambherao@gmail.com>
2026-07-11 02:58:24 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin:"https://ai-resume-job-analyze-frontend.onrender.com" , 
-    credentials:true 
+    credentials:true ,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'] 
 }))
 
 
